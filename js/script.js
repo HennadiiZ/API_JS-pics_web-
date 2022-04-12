115abf70af71671ca03747b3e93d0ca9990ab545
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-04-12 21:27:49 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,44 +1,45 @@
 
+
 // https://www.pexels.com
 // https://www.pexels.com/ru-ru/api/new/
 //API key: 563492ad6f917000010000015b13eef540a24bde9cb605b7aa59d050 
 
 
-const auth = "563492ad6f917000010000015b13eef540a24bde9cb605b7aa59d050";
-const gallery = document.querySelector('.gallery');
-const searchInput = document.querySelector('.search-input');
-// const submitButton = document.querySelector('.submit-btn');
-const form = document.querySelector('.search-form');
-let searchValue;
+// const auth = "563492ad6f917000010000015b13eef540a24bde9cb605b7aa59d050";
+// const gallery = document.querySelector('.gallery');
+// const searchInput = document.querySelector('.search-input');
+// // const submitButton = document.querySelector('.submit-btn');
+// const form = document.querySelector('.search-form');
+// let searchValue;
 
 // curl -H "Authorization: 563492ad6f917000010000015b13eef540a24bde9cb605b7aa59d050" \
 // "https://api.pexels.com/v1/curated?per_page=1"
 // "https://api.pexels.com/v1/curated?per_page=15&page=1"
+//-1
+// async function curatedPhotos(){
 
-async function curatedPhotos(){
+//     const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
+//       method: 'GET',
+//       headers: {
+//         Accept: 'application/json',
+//         Authorization: auth
+//       }
+//     });
 
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        Authorization: auth
-      }
-    });
-
-    const data = await dataFetch.json();
-    //console.log(data); // {page: 1, per_page: 15, photos: Array(15), total_results: 8000, next_page: 'https://api.pexels.com/v1/curated/?page=2&per_page=15'}
-    //console.log(data.photos);
-    data.photos.forEach(photo=>{
-      const galleryImg = document.createElement('div');
-      galleryImg.classList.add('gallery-img');
-      galleryImg.innerHTML = `
-      <img src=${photo.src.large}></img>
-      <p>${photo.photographer}</p>
-      `;
-      gallery.append(galleryImg);
-    })
-}
-curatedPhotos();
+//     const data = await dataFetch.json();
+//     //console.log(data); // {page: 1, per_page: 15, photos: Array(15), total_results: 8000, next_page: 'https://api.pexels.com/v1/curated/?page=2&per_page=15'}
+//     //console.log(data.photos);
+//     data.photos.forEach(photo=>{
+//       const galleryImg = document.createElement('div');
+//       galleryImg.classList.add('gallery-img');
+//       galleryImg.innerHTML = `
+//       <img src=${photo.src.large}></img>
+//       <p>${photo.photographer}</p>
+//       `;
+//       gallery.append(galleryImg);
+//     })
+// }
+// curatedPhotos();
 
 
 // 
@@ -46,10 +47,59 @@ curatedPhotos();
 // "https://api.pexels.com/v1/search?query=nature&per_page=1"
 //
 
+// searchInput.addEventListener('input', updateInput);
+// function updateInput(e){
+//     // console.log(e);
+//     // console.log(e.target.value);
+//     searchValue = e.target.value;
+// }
+
+// form.addEventListener('submit', (e)=>{
+//   e.preventDefault();
+//   searchPhotos(searchValue);
+//   form.reset();
+// });
+
+
+//-2
+// async function searchPhotos(query){
+//   const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}+query&per_page=1`, {
+//     method: 'GET',
+//     headers: {
+//       Accept: 'application/json',
+//       Authorization: auth
+//     }
+//   });
+//   // console.log(dataFetch);
+//   const data = await dataFetch.json();
+//   //console.log(data); // {page: 1, per_page: 1, photos: Array(0), total_results: 0}
+
+//   data.photos.forEach(photo=>{
+//     const galleryImg = document.createElement('div');
+//     galleryImg.classList.add('gallery-img');
+//     galleryImg.innerHTML = `
+//     <img src=${photo.src.large}></img>
+//     <p>${photo.photographer}</p>
+//     `;
+//     gallery.append(galleryImg);
+//   })
+// }
+// searchPhotos(searchValue)
+
+
+
+
+
+
+// refactoring =================================================================
+const auth = "563492ad6f917000010000015b13eef540a24bde9cb605b7aa59d050";
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-input');
+const form = document.querySelector('.search-form');
+let searchValue;
+
 searchInput.addEventListener('input', updateInput);
 function updateInput(e){
-    // console.log(e);
-    // console.log(e.target.value);
     searchValue = e.target.value;
 }
 
@@ -59,17 +109,42 @@ form.addEventListener('submit', (e)=>{
   form.reset();
 });
 
-async function searchPhotos(query){
-  const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}+query&per_page=1`, {
+async function fetchApi(url){
+  const dataFetch = await fetch(url , {
     method: 'GET',
     headers: {
       Accept: 'application/json',
       Authorization: auth
     }
   });
-  // console.log(dataFetch);
+
   const data = await dataFetch.json();
-  //console.log(data); // {page: 1, per_page: 1, photos: Array(0), total_results: 0}
+  return data;
+}
+
+//-1
+async function curatedPhotos(){
+  const data = await fetchApi(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1"
+  );
+
+  data.photos.forEach(photo=>{
+    const galleryImg = document.createElement('div');
+    galleryImg.classList.add('gallery-img');
+    galleryImg.innerHTML = `
+    <img src=${photo.src.large}></img>
+    <p>${photo.photographer}</p>
+    `;
+    gallery.append(galleryImg);
+  });
+}
+curatedPhotos();
+
+//-2
+async function searchPhotos(query){
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=1`
+  );
 
   data.photos.forEach(photo=>{
     const galleryImg = document.createElement('div');
@@ -81,4 +156,5 @@ async function searchPhotos(query){
     gallery.append(galleryImg);
   })
 }
-// searchPhotos(searchValue)
+
+//  =================================================================
